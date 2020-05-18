682ddf37593f73e3737819ca0fe3e807c6d64b41
Abhishek Jain <1709abhishek@gmail.com>
2020-05-18 10:13:14 +0530

    added reducer for auth

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,13 +4,31 @@ export class Login extends Component {
     constructor(props) {
         super(props)
 
-        this.emailInputRef = React.createRef();
-        this.passwordInputRef = React.createRef();
+        // this.emailInputRef = React.createRef();
+        // this.passwordInputRef = React.createRef();
+        this.state = {
+            email: '',
+            password: ''
+        }
     }
 
     handleFormSubmit = (e) => {
         e.preventDefault();
-        console.log('this.emailInputRef', this.emailInputRef);
+        console.log(this.state);
+    }
+
+    handleEmailChange = (e) => {
+        e.preventDefault();
+        this.setState({
+            email: e.target.value
+        })
+    }
+
+    handlePasswordChange = (e) => {
+        e.preventDefault();
+        this.setState({
+            password: e.target.value
+        })
     }
 
     render() {
@@ -18,10 +36,22 @@ export class Login extends Component {
             <form className="login-form">
                 <span className="login-signup-header">Log In</span>
                 <div className="field">
-                    <input type="email" placeholder="Email" required ref={this.emailInputRef} />
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        required
+                        onChange={this.handleEmailChange}
+                        value={this.state.email}
+                    />
                 </div>
                 <div className="field">
-                    <input type="password" placeholder="Password" required ref={this.passwordInputRef} />
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        required
+                        onChange={this.handlePasswordChange}
+                        value={this.state.password}
+                    />
                 </div>
                 <div className="field">
                     <button onClick={this.handleFormSubmit}>Log In</button>
